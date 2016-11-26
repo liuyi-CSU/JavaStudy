c404dd8403086d58632d4454cbc416a69d13a0bc
liuyi <408827065@qq.com>
2016-11-27 00:06:34 +0800

    init

---
+++ HelloWorld/src/com/liuyi/demo/TestGit.java
@@ -0,0 +1,13 @@
+package com.liuyi.demo;
+
+public class TestGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my first time");
+	}
+
+}
